20d14928f5b3eeb13e988823e178a7d8ecb23c6b
Ana Roman <nanirom@hotmail.com>
2022-04-11 14:35:27 +0200

    Segundo comiteo

--- ProyectoJavaGit/src/proyectojavagit/ProyectoJavaGit.java
+++ ProyectoJavaGit/src/proyectojavagit/ProyectoJavaGit.java
@@ -18,6 +18,7 @@ public class ProyectoJavaGit {
         // TODO code application logic here
         
         System.out.println("Hola Mundo!");
+        System.out.println("Qué tal todo?");
     }
     
 }
